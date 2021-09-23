2c6726645396cb6ca86d9f33bb3e6008c213471c
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-09-23 18:23:28 -0300

    CODING PART 1 - FIRST 2 API ENDPOINTS (GET / READ)

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers 
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase 
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands() 
+        {
+            var commandItens = _repository.GetAllCommands();
+
+            return Ok(commandItens);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id) 
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Study Project</h1>
+
+<p align="center">🚀 Command API </p>
+
+### 💻 About
+
+In this full course, we show you how to build a full REST API using .NET Core 3.1. We’ll employ MVC, REST, the Repository Pattern, Dependency Injection, Entity Framework, Data Transfer Objects, (DTOs), AutoMapper to provide 6 API endpoints that will allow you to Create, Read Update and Delete resources.
+
+### ⌨️ Execution
+
+dotnet run
+
+### 🛠 Features
+
+
+### ⚙️ Requirements
+
+
+### :bust_in_silhouette: Author
+
+* Fernanda Pereira - [ferprr](https://github.com/ferprr)
