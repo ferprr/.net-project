d2ca2235bc845af5004ab8d17ab45a77644fcfac
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-09-23 22:05:38 -0300

    DONE: CODING PART 2 - ENTITY FRAMEWORK & SQL SERVER AND CODING PART 3 - DATA TRANSFER OBJECTS & CREATE ENDPOINT

--- Data/ICommanderRepo.cs
+++ Data/ICommanderRepo.cs
@@ -2,9 +2,11 @@ using System.Collections.Generic;
 using Commander.Models;
 
 namespace Commander.Data {
-    public interface ICommanderRepo {
+    public interface ICommanderRepo 
+    {
+        bool SaveChanges();
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
-
+        void CreateCommand(Command cmd);
     }
 }
